052cf60908a13f508883ade236344ae3f34c3e7e
Fabrice Cheng <fabrice.cheng@gmail.com>
2025-04-26 08:44:39 -0700

    add height

--- app/proposals/[proposalId]/page.tsx
+++ app/proposals/[proposalId]/page.tsx
@@ -10,23 +10,30 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useAccount } from "wagmi";
 import { Share2, Feather as Ethereum, Users, Diamond } from "lucide-react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { formatNumberWithCommas, shortenAddress, timeAgo } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
 import { useSignMessage } from "wagmi";
 import { useAppKit } from "@reown/appkit/react";
 
-import { useParams } from 'next/navigation'
+import { useParams } from "next/navigation";
 import { UserVote } from "@/interfaces/UserVote";
 import { Proposal } from "@/interfaces/Proposal";
-import { calculateTotalVoters, calculateTotalVotes } from "@/helpers/voteAggregation";
+import {
+  calculateTotalVoters,
+  calculateTotalVotes,
+} from "@/helpers/voteAggregation";
 import { VotesByETH } from "@/components/VotesByETH";
 import { Vote } from "@/interfaces/Vote";
 import { VotesByNumber } from "@/components/VotesByNumber";
 
 export default function ProposalPage() {
-
-  const params = useParams()
+  const params = useParams();
   const proposalId = params.proposalId as string;
 
   const [showVoteDialog, setShowVoteDialog] = useState(false);
@@ -49,16 +56,16 @@ export default function ProposalPage() {
         toast({
           title: "Error fetching proposal",
           description: data.error,
-          variant: "destructive"
-        })
+          variant: "destructive",
+        });
         return;
       }
 
       setProposal(data);
-    }
+    };
 
     fetchProposal();
-  }, [proposalId])
+  }, [proposalId]);
 
   useEffect(() => {
     const fetchUserVote = async () => {
@@ -70,8 +77,8 @@ export default function ProposalPage() {
             toast({
               title: "Error fetching user's vote",
               description: response.statusText,
-              variant: "destructive"
-            })
+              variant: "destructive",
+            });
             return;
           }
 
@@ -81,15 +88,14 @@ export default function ProposalPage() {
           toast({
             title: "Error fetching user's vote",
             description: "",
-            variant: "destructive"
-          })
+            variant: "destructive",
+          });
         }
       }
     };
 
     fetchUserVote();
   }, [isConnected, address, proposalId, userVote?.voteOption]);
-
 
   useEffect(() => {
     const fetchRecentVotes = async () => {
@@ -100,20 +106,19 @@ export default function ProposalPage() {
           toast({
             title: "Error fetching recent votes",
             description: response.statusText,
-            variant: "destructive"
-          })
+            variant: "destructive",
+          });
           return;
         }
 
         const data = await response.json();
         setRecentVotes(data);
-
       } catch (error) {
         toast({
           title: "Error fetching recent votes",
           description: "",
-          variant: "destructive"
-        })
+          variant: "destructive",
+        });
       }
     };
 
@@ -157,8 +162,8 @@ export default function ProposalPage() {
         toast({
           title: "Error voting",
           description: result.error,
-          variant: "destructive"
-        })
+          variant: "destructive",
+        });
         return;
       }
       setUserVote({
@@ -170,8 +175,8 @@ export default function ProposalPage() {
       toast({
         title: "Error voting",
         description: "",
-        variant: "destructive"
-      })
+        variant: "destructive",
+      });
     } finally {
       setIsLoading(false);
     }
@@ -184,7 +189,7 @@ export default function ProposalPage() {
     const tweetText = `I voted "${userVote.voteOption}" for "${proposal?.description}".\n\nhttps://www.ethpulse.io/proposals/${proposalId}`;
     const encodedTweet = encodeURIComponent(tweetText);
     const twitterUrl = `https://twitter.com/intent/tweet?text=${encodedTweet}`;
-    window.open(twitterUrl, '_blank', 'noopener,noreferrer');
+    window.open(twitterUrl, "_blank", "noopener,noreferrer");
   };
 
   return (
@@ -195,18 +200,24 @@ export default function ProposalPage() {
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
-
           <div className="bg-white rounded-xl shadow-md p-6 border border-gray-100">
             <h3 className="text-lg font-semibold mb-4">Vote Distribution</h3>
             <div className="space-y-6">
               <div className="text-center">
                 <div className="flex items-center justify-center mb-2">
                   <Diamond className="h-6 w-6 text-blue-500 mr-2" />
-                  <span className="text-2xl font-bold">{formatNumberWithCommas(proposal ? calculateTotalVotes(proposal?.aggregateVote) : 0)} ETH</span>
+                  <span className="text-2xl font-bold">
+                    {formatNumberWithCommas(
+                      proposal
+                        ? calculateTotalVotes(proposal?.aggregateVote)
+                        : 0
+                    )}{" "}
+                    ETH
+                  </span>
                 </div>
                 <p className="text-gray-600">Total ETH Votes</p>
               </div>
-              <div className="h-64">
+              <div className="h-32">
                 {proposal && <VotesByETH proposal={proposal} />}
               </div>
             </div>
@@ -218,22 +229,27 @@ export default function ProposalPage() {
               <div className="text-center">
                 <div className="flex items-center justify-center mb-2">
                   <Users className="h-6 w-6 text-blue-500 mr-2" />
-                  <span className="text-2xl font-bold">{proposal ? calculateTotalVoters(proposal?.aggregateVote) : 0}</span>
+                  <span className="text-2xl font-bold">
+                    {proposal
+                      ? calculateTotalVoters(proposal?.aggregateVote)
+                      : 0}
+                  </span>
                 </div>
                 <p className="text-gray-600">Total Voters</p>
               </div>
-              <div className="h-64">
+              <div className="h-32">
                 {proposal && <VotesByNumber proposal={proposal} />}
               </div>
             </div>
           </div>
         </div>
 
-
         {userVote ? (
           <div className="rounded-lg text-center gap-4 pb-6">
             <h3 className="text-xl mb-4 text-gray-600">
-              You voted <span className="text-green-500">{userVote.voteOption}</span> with {parseFloat(userVote.numVotes ?? "0").toFixed(5)} ETH
+              You voted{" "}
+              <span className="text-green-500">{userVote.voteOption}</span> with{" "}
+              {parseFloat(userVote.numVotes ?? "0").toFixed(5)} ETH
             </h3>
             <Button
               onClick={handleShareTwitter}
@@ -244,25 +260,25 @@ export default function ProposalPage() {
           </div>
         ) : (
           <div className="flex gap-6 justify-center mb-12">
-            {proposal && proposal.options.map((option, index) => (
-              <Button
-                key={index}
-                onClick={() => handleVote(option)}
-                className="group relative flex items-center overflow-hidden bg-white hover:bg-gray-50 text-gray-800 px-10 py-6 text-lg rounded-2xl shadow-lg transition-all duration-300 border-2 border-gray-200 hover:border-gray-300"
-                disabled={isLoading}
-              >
-                <div className="absolute inset-0 bg-gradient-to-r from-white/0 via-gray-100/20 to-white/0 translate-x-[-200%] group-hover:translate-x-[200%] transition-transform duration-700" />
-                <span className="font-medium tracking-wide">Vote {option}</span>
-              </Button>
-
-            ))}
+            {proposal &&
+              proposal.options.map((option, index) => (
+                <Button
+                  key={index}
+                  onClick={() => handleVote(option)}
+                  className="group relative flex items-center overflow-hidden bg-white hover:bg-gray-50 text-gray-800 px-10 py-6 text-lg rounded-2xl shadow-lg transition-all duration-300 border-2 border-gray-200 hover:border-gray-300"
+                  disabled={isLoading}
+                >
+                  <div className="absolute inset-0 bg-gradient-to-r from-white/0 via-gray-100/20 to-white/0 translate-x-[-200%] group-hover:translate-x-[200%] transition-transform duration-700" />
+                  <span className="font-medium tracking-wide">
+                    Vote {option}
+                  </span>
+                </Button>
+              ))}
           </div>
         )}
 
         <div className="bg-blue-50 rounded-lg p-6 border border-blue-100">
-          <h2 className="text-xl font-semibold mb-4">
-            Recent Votes
-          </h2>
+          <h2 className="text-xl font-semibold mb-4">Recent Votes</h2>
           <div className="space-y-4">
             {recentVotes.map((vote, index) => (
               <div
@@ -277,14 +293,15 @@ export default function ProposalPage() {
                     <span className="block sm:hidden">
                       {shortenAddress(vote.wallet, 4)}
                     </span>
-                    <span className="hidden sm:block">{shortenAddress(vote.wallet, 4)}</span>
+                    <span className="hidden sm:block">
+                      {shortenAddress(vote.wallet, 4)}
+                    </span>
                   </span>
                 </div>
 
                 {/* Vote and ETH Section */}
                 <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-4">
-                  <span
-                    className="text-sm sm:text-base font-medium text-green-500">
+                  <span className="text-sm sm:text-base font-medium text-green-500">
                     {vote.voteOption}
                   </span>
                   <span className="text-gray-600 text-sm sm:text-base">
@@ -299,16 +316,24 @@ export default function ProposalPage() {
           </div>
         </div>
 
-
         <div className="mt-8">
-          <h2 className="text-2xl font-semibold mb-6 text-center">Frequently Asked Questions</h2>
+          <h2 className="text-2xl font-semibold mb-6 text-center">
+            Frequently Asked Questions
+          </h2>
           <Accordion type="single" collapsible className="w-full">
             <AccordionItem value="item-2">
               <AccordionTrigger className="text-left">
                 How is my vote weighted?
               </AccordionTrigger>
               <AccordionContent>
-                Votes are weighted based on the amount of ETH, ETH staked (beaconchain), ETH derivates in your connected wallet at the time of voting. The following blockchains are supported: $ETH (Ethereum, Base, Optimism, Arbitrum, zkSync, Linea), WETH (L1, Base, Optimism, Arbitrum), rETH (L1), stETH (L1, Base, Arbitrum, Optimism), Aave ETH (L1), Aave stETH (L1), Aave eETH (L1). This ensures that stakeholders with greater investments in the Ethereum ecosystem have a proportional influence on the outcome.
+                Votes are weighted based on the amount of ETH, ETH staked
+                (beaconchain), ETH derivates in your connected wallet at the
+                time of voting. The following blockchains are supported: $ETH
+                (Ethereum, Base, Optimism, Arbitrum, zkSync, Linea), WETH (L1,
+                Base, Optimism, Arbitrum), rETH (L1), stETH (L1, Base, Arbitrum,
+                Optimism), Aave ETH (L1), Aave stETH (L1), Aave eETH (L1). This
+                ensures that stakeholders with greater investments in the
+                Ethereum ecosystem have a proportional influence on the outcome.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
@@ -323,18 +348,20 @@ export default function ProposalPage() {
                   rel="noopener noreferrer"
                   className="text-blue-500 hover:text-blue-600 transition-colors"
                 >
-                  here</a>.<br />
-                It only requires a wallet signature.
-                Signing this transaction is free and will not cost you any gas.
+                  here
+                </a>
+                .<br />
+                It only requires a wallet signature. Signing this transaction is
+                free and will not cost you any gas.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
               <AccordionTrigger className="text-left">
-
                 Do I need to pay any gas to vote?
               </AccordionTrigger>
               <AccordionContent>
-                No, voting is completely free and does not require sending any transaction on-chain.
+                No, voting is completely free and does not require sending any
+                transaction on-chain.
               </AccordionContent>
             </AccordionItem>
           </Accordion>
@@ -348,7 +375,9 @@ export default function ProposalPage() {
           </DialogHeader>
           <div className="bg-blue-50 p-6 rounded-lg text-center">
             <h3 className="text-xl mb-4">
-              You voted <span className="text-green-500">{userVote?.voteOption}</span> for "{proposal?.description}".
+              You voted{" "}
+              <span className="text-green-500">{userVote?.voteOption}</span> for
+              "{proposal?.description}".
             </h3>
             <Button
               onClick={handleShareTwitter}
@@ -359,8 +388,6 @@ export default function ProposalPage() {
           </div>
         </DialogContent>
       </Dialog>
-    </div >
+    </div>
   );
 }
-
-
