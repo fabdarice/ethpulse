33baadc7161bce98fb12dbd25ff4c96aa3e2a80e
Fabrice Cheng <fabrice.cheng@gmail.com>
2025-04-27 21:18:04 -0700

    push

--- lib/alchemy.ts
+++ lib/alchemy.ts
@@ -17,39 +17,41 @@ const networks = [
 // Define the ERC20 tokens to include with their contract addresses per network
 const tokensPerNetwork: {
   [key: string]: {
-    stETH?: string;      // Lido ETH
-    wstETH?: string;      // wrapped Lido ETH
-    rETH?: string;       // Rocket Pool ETH
-    WETH?: string;       // Wrapped ETH
-    aEthWETH?: string;       // Aave Wrapped ETH
+    stETH?: string; // Lido ETH
+    wstETH?: string; // wrapped Lido ETH
+    rETH?: string; // Rocket Pool ETH
+    WETH?: string; // Wrapped ETH
+    aEthWETH?: string; // Aave Wrapped ETH
     aEthwstETH?: string;
     aEthweETH?: string;
   };
 } = {
   [Network.ETH_MAINNET]: {
     stETH: "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84", // Lido stETH
-    rETH: "0xae78736Cd615f374D3085123A210448E74Fc6393",  // Rocket Pool rETH
-    WETH: "0xC02aaA39b223FE8D0A0E5C4F27eAD9083C756Cc2",  // WETH
+    rETH: "0xae78736Cd615f374D3085123A210448E74Fc6393", // Rocket Pool rETH
+    WETH: "0xC02aaA39b223FE8D0A0E5C4F27eAD9083C756Cc2", // WETH
     aEthWETH: "0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8", // Aave WETH
     aEthwstETH: "0x0B925eD163218f6662a35e0f0371Ac234f9E9371", // Aave Lido stETH
-    aEthweETH: "0xBdfa7b7893081B35Fb54027489e2Bc7A38275129" // Aave EtherFI ETH
+    aEthweETH: "0xBdfa7b7893081B35Fb54027489e2Bc7A38275129", // Aave EtherFI ETH
+    wstETH: "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0", // Lido wstETH
   },
   [Network.BASE_MAINNET]: {
     wstETH: "0xc1CBa3fCea344f92D9239c08C0568f6F2F0ee452", // lido wstETH
-    WETH: "0x4200000000000000000000000000000000000006"
+    WETH: "0x4200000000000000000000000000000000000006",
   }, // Add tokens for Base
   [Network.ARB_MAINNET]: {
     wstETH: "0x5979D7b546E38E414F7E9822514be443A4800529", // lido wstETH
-    WETH: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1"
-  }, // Add tokens for Arbitrum 
+    WETH: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+  }, // Add tokens for Arbitrum
   [Network.OPT_MAINNET]: {
     wstETH: "0x1F32b1c2345538c0c6f582fCB022739c4A194Ebb", // lido wstETH
-    WETH: "0x4200000000000000000000000000000000000006"
-  }, // Add tokens for OP 
-
+    WETH: "0x4200000000000000000000000000000000000006",
+  }, // Add tokens for OP
 };
 
-export const getETHBalanceAllNetworks = async (walletAddress: string): Promise<bigint> => {
+export const getETHBalanceAllNetworks = async (
+  walletAddress: string
+): Promise<bigint> => {
   const alchemyInstances = networks.map(({ name, value }) => ({
     name,
     alchemy: new Alchemy({
@@ -67,7 +69,9 @@ export const getETHBalanceAllNetworks = async (walletAddress: string): Promise<b
         const nativeBalanceBigInt = BigInt(nativeBalance.toString());
 
         // Fetch ERC20 token balances
-        const tokenAddresses = Object.values(tokensPerNetwork[networkValue] || {}).filter(addr => addr !== "0x...");
+        const tokenAddresses = Object.values(
+          tokensPerNetwork[networkValue] || {}
+        ).filter((addr) => addr !== "0x...");
         let tokenBalancesBigInt: bigint = BigInt(0);
 
         if (tokenAddresses.length > 0) {
@@ -77,8 +81,13 @@ export const getETHBalanceAllNetworks = async (walletAddress: string): Promise<b
           );
 
           // Iterate through each token balance and sum them
-          tokenBalancesResponse.tokenBalances.forEach(token => {
-            if (token && token.tokenBalance && token.tokenBalance !== "0x0000000000000000000000000000000000000000000000000000000000000000") {
+          tokenBalancesResponse.tokenBalances.forEach((token) => {
+            if (
+              token &&
+              token.tokenBalance &&
+              token.tokenBalance !==
+                "0x0000000000000000000000000000000000000000000000000000000000000000"
+            ) {
               tokenBalancesBigInt += BigInt(token.tokenBalance);
             }
           });
@@ -99,10 +108,9 @@ export const getETHBalanceAllNetworks = async (walletAddress: string): Promise<b
 
     const validatorBalance = await getValidatorBalance(walletAddress);
 
-    return (totalBalance + validatorBalance);
+    return totalBalance + validatorBalance;
   } catch (error) {
     console.error("getETHBalanceAllNetworks error", error);
-    throw error
+    throw error;
   }
 };
-
